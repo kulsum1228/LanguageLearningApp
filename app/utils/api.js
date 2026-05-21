a9cbdeb872c35dcd71d4a8447ea8d7f22cc2e526
Umme Kulsum <ummekulsum0803@gmail.com>
2026-05-21 09:28:30 +0530

    Switch back to local backend for APK demo

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://languagelearningapp-production-1e5e.up.railway.app/api",
+  // baseURL: "https://languagelearningapp-production-1e5e.up.railway.app/api",
+  baseURL: "http://100.64.5.64:5000/api",
 });
 
 export default API;
